50482b308e66314734fc7f481f5dd49028bc609c
ShannonPaige <spaige@gmail.com>
2016-02-17 11:49:30 -0700

    Add test for user sign in

--- spec/features/user_can_sign_in_spec.rb
+++ spec/features/user_can_sign_in_spec.rb
@@ -6,7 +6,7 @@ Rails.describe "the signin process", :type => :feature do
     Rails.application.env_config["omniauth.auth"] = OmniAuth.config.mock_auth[:instagram]
   end
 
-  it "signs me in" do
+  it "signs in with Instagram oauth" do
     visit '/'
     click_link 'Login with Instagram'
     expect(page).to have_content 'You Are Authorized ig_name ig_nickname ig_email ig_bio ig_website Logout'
